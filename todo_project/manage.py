ebed8aa42646a6e7d43c9be44615a7894c3234b8
Fatih Arslan <fatiharslan748@gmail.com>
2022-03-07 19:13:04 +0300

    added x

--- todo_project/manage.py
+++ todo_project/manage.py
@@ -5,6 +5,7 @@ import sys
 
 
 def main():
+    x = 5
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo_project.settings')
     try:
         from django.core.management import execute_from_command_line
